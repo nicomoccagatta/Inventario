3c3b9ab17055dc301a562df002cdf6cc0c5613b1
nicomoccagatta <nicomoccagatta@gmail.com>
2015-06-17 10:45:55 -0300

    [ADMIN]Ultimos cambios e interfaz andando!

--- Administrador/VistaAdmin/VistaVentanaPPAL.cpp
+++ Administrador/VistaAdmin/VistaVentanaPPAL.cpp
@@ -7,10 +7,81 @@
 
 #include "VistaVentanaPPAL.h"
 
-VistaVentanaPPAL::VistaVentanaPPAL(Modelo_Observable *modelo) {
+VistaVentanaPPAL::VistaVentanaPPAL(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade)
+: Gtk::Window(cobject), model(0), m_refGlade(refGlade),listado_productos(0),agregar_productos(0),
+  listado_av(0),agregar_av(0),sgp(0),spav(0),shpp(0),panelDinamico(0)
+{
+	m_refGlade->get_widget("Listado de Productos", listado_productos);
+	if(listado_productos)
+		listado_productos->signal_clicked().connect( sigc::mem_fun(*this, &VistaVentanaPPAL::on_button_LP) );
+
+	m_refGlade->get_widget("Agregar Producto", agregar_productos);
+		if(agregar_productos)
+			agregar_productos->signal_clicked().connect( sigc::mem_fun(*this, &VistaVentanaPPAL::on_button_AP) );
+
+	m_refGlade->get_widget("Listado de Areas de Vision", listado_av);
+		if(listado_av)
+			listado_av->signal_clicked().connect( sigc::mem_fun(*this, &VistaVentanaPPAL::on_button_LAV) );
+
+
+	m_refGlade->get_widget("Agregar Areas de Vision", agregar_av);
+		if(agregar_av)
+			agregar_av->signal_clicked().connect( sigc::mem_fun(*this, &VistaVentanaPPAL::on_button_AAV) );
+
+	m_refGlade->get_widget("SGPP", sgp);
+		if(sgp)
+			sgp->signal_clicked().connect( sigc::mem_fun(*this, &VistaVentanaPPAL::on_button_SGPP) );
+
+	m_refGlade->get_widget("SPAV", spav);
+		if(spav)
+			spav->signal_clicked().connect( sigc::mem_fun(*this, &VistaVentanaPPAL::on_button_SPAV) );
+
+	m_refGlade->get_widget("SHPP", shpp);
+		if(shpp)
+			shpp->signal_clicked().connect( sigc::mem_fun(*this, &VistaVentanaPPAL::on_button_SHPP) );
+
+	m_refGlade->get_widget("panelDinamico",panelDinamico);
+}
+
+void VistaVentanaPPAL::setearAtributos(const Glib::RefPtr<Gtk::Builder>& refGlade,Modelo_Observable *modelo){
+	m_refGlade = refGlade;
 	model = modelo;
 }
 
 VistaVentanaPPAL::~VistaVentanaPPAL() {
 }
 
+void VistaVentanaPPAL::on_button_LP()
+{
+  hide();
+}
+
+void VistaVentanaPPAL::on_button_AP()
+{
+  hide();
+}
+
+void VistaVentanaPPAL::on_button_LAV()
+{
+  hide();
+}
+
+void VistaVentanaPPAL::on_button_AAV()
+{
+  hide();
+}
+
+void VistaVentanaPPAL::on_button_SGPP()
+{
+  hide();
+}
+
+void VistaVentanaPPAL::on_button_SPAV()
+{
+  hide();
+}
+
+void VistaVentanaPPAL::on_button_SHPP()
+{
+  hide();
+}
--- Administrador/VistaAdmin/VistaVentanaPPAL.h
+++ Administrador/VistaAdmin/VistaVentanaPPAL.h
@@ -8,16 +8,30 @@
 #ifndef ADMINISTRADOR_VISTAADMIN_VISTAVENTANAPPAL_H_
 #define ADMINISTRADOR_VISTAADMIN_VISTAVENTANAPPAL_H_
 
-#include <gtkmm-2.4/gtkmm/window.h>
+#include <gtkmm-2.4/gtkmm.h>
 #include "ModeloAdmin/admin_ModeloObservable.h"
 #include "ModeloAdmin/admin_Observer.h"
 
-class VistaVentanaPPAL: public Gtk::Window, public Observer {
+class VistaVentanaPPAL: public Gtk::Window, Observer {
 public:
-	VistaVentanaPPAL(Modelo_Observable *modelo);
+	VistaVentanaPPAL(BaseObjectType* cobject,const Glib::RefPtr<Gtk::Builder>& refGlade);
+	void setearAtributos(const Glib::RefPtr<Gtk::Builder>& refGlade,Modelo_Observable *modelo);
 	virtual ~VistaVentanaPPAL();
 private:
 	Modelo_Observable *model;
+	void on_button_LP();
+	void on_button_AP();
+	void on_button_LAV();
+	void on_button_AAV();
+	void on_button_SGPP();
+	void on_button_SPAV();
+	void on_button_SHPP();
+
+	Glib::RefPtr<Gtk::Builder> m_refGlade;
+	Gtk::Button *listado_productos,*agregar_productos;
+	Gtk::Button *listado_av,*agregar_av;
+	Gtk::Button *sgp,*spav,*shpp;
+	Gtk::Viewport *panelDinamico;
 };
 
 #endif /* ADMINISTRADOR_VISTAADMIN_VISTAVENTANAPPAL_H_ */
